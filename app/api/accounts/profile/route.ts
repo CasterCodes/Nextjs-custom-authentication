df6fba317844d96e95881e144a191a8f98d2af21
CasterCodes <nyagucha.kevin.otwori@gmail.com>
2024-01-30 17:35:10 +0300

    fix: add trycatch to get profile route

--- app/api/accounts/profile/route.ts
+++ app/api/accounts/profile/route.ts
@@ -1,37 +1,56 @@
 import { isAuthenticatedOrAuthorized } from "@/app/lib/account";
-import { getAccessToken, verifyToken } from "@/app/lib/jwt";
 import User from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const { authenticated, message, id } = await isAuthenticatedOrAuthorized([
-    "user",
-    "admin",
-  ]);
+  try {
+    const { authenticated, message, id } = await isAuthenticatedOrAuthorized([
+      "user",
+      "admin",
+    ]);
 
-  if (!authenticated || !id) {
+    if (!authenticated || !id) {
+      return NextResponse.json(
+        {
+          status: "fail",
+          message: message,
+        },
+        { status: 401 }
+      );
+    }
+
+    if (!authenticated) {
+      return NextResponse.json({
+        user: null,
+        error: true,
+        message: message,
+      });
+    }
+
+    const user = await User.findById(id).select("-password");
+
+    if (!user) {
+      return NextResponse.json({
+        status: "fail",
+        message: "No user",
+      });
+    }
+
+    return NextResponse.json(
+      {
+        status: "success",
+        message: "Logged in profile",
+        user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
     return NextResponse.json(
       {
         status: "fail",
-        message: message,
+        message: "Something went wrong",
       },
-      { status: 401 }
+      { status: 500 }
     );
   }
-
-  if (!authenticated) {
-    return {
-      user: null,
-      error: true,
-      message: message,
-    };
-  }
-
-  const user = await User.findById(id).select("-password");
-
-  return NextResponse.json({
-    status: "success",
-    message: "Logged in profile",
-    user,
-  });
 }
